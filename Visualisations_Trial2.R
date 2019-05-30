7c61b7229ea75fc0b11a20b711a2e8f7d4e91426
gijsbertwerner <gijsbert.werner@zoo.ox.ac.uk>
2019-05-30 12:23:10 +0200

    Data cleaning for second run done.

--- Visualisations_Trial2.R
+++ Visualisations_Trial2.R
@@ -15,17 +15,47 @@ library(corHMM)
 
 #####Data reading
 
+#Read the tree
+tree<-read.tree("./Data/v0.1/ALLMB.tre")
+tree
+
 #Read in data directly from the zipped file
 dat<-as.data.table(
   read.csv(unz("./Data/ALLMB_Consolidated_FleshedGenera_TRY_Traits.csv.zip",
              "ALLMB_Consolidated_FleshedGenera_TRY_Traits.csv")))
 head(dat)
+nrow(dat)
 
 dat_gf<-as.data.table(
   read.csv(unz("./Data/ALLMB_Consolidated_FleshedGenera_TRY_Traits_GIFT_GrowthForm.csv.zip",
                "ALLMB_Consolidated_FleshedGenera_TRY_Traits_GIFT_GrowthForm.csv")))
 head(dat_gf)
+nrow(dat_gf) #Data not present for all tree tips, about 150k fewer. All missing ones treat as NA?
 
-tree<-read.tree("./Data/v0.1/ALLMB.tre")
+
+##Have a look at the data
+
+#We'll use ALLMB_OrigSpecies for matching. 
+dat$match_col<-gsub(pattern=" ","_",dat$ALLMB_OrigSpecies)
+#Are the all present in the tree, and vice versa? 
+length(which(dat$match_col %in% tree$tip.label))
+length(which(tree$tip.label %in% dat$match_col))
+dat[which(!dat$match_col %in% tree$tip.label),]
+
+#Ok, so there about 142 non-overlapping species names. These are spelling/typo problems. We will drop these. 
+dat<-dat %>% filter(dat$match_col %in% tree$tip.label)
+tree<-drop.tip(tree,tree$tip.label[!tree$tip.label %in% dat$match_col])
 tree
 
+#Now consider the growth form dataset
+table(dat_gf$GIFT_PlantGrowthForm,useNA = "ifany")
+#What column to match them by? There appears to not be the ALLMB_OrigSpecies present
+head(dat_gf)
+names(dat_gf)
+#We'll use TRY_ACC_SpeciesID to match
+dat$GIFT_PlantGrowthForm<-
+  dat_gf$GIFT_PlantGrowthForm[match(dat$TRY_AccSpeciesID,dat_gf$TRY_AccSpeciesID)]
+table(dat$GIFT_PlantGrowthForm,useNA = "ifany")
+#Ok, this looks all good
+rm(dat_gf)
+gc()
